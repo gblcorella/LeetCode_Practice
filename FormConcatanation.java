0d1695e13741535890550081247a624044b8793c
Gabe Corella <69766707+gblcorella@users.noreply.github.com>
2021-01-02 17:14:02 -0500

    corrected import

--- FormConcatanation.java
+++ FormConcatanation.java
@@ -9,5 +9,28 @@
  * @author gabecorella
  */
 public class FormConcatanation {
-    
+    public boolean canFormArray(int[] arr, int[][] pieces) {
+        Map<Integer,int[]> map = new HashMap();
+        int start = 0; 
+        
+        for(int[] piece : pieces){
+            map.put(piece[0],piece);
+        }
+        
+        while (start < arr.length){
+            if(map.containsKey(arr[start])){
+                int[] curr = map.get(arr[start]);
+                for(int i = 0; i < curr.length; i++){
+                    if(arr[start] != curr[i]){
+                        return false;
+                    } else {
+                        start++;
+                    }
+                }
+            } else{
+                return false;
+            }
+        }
+        return true; 
+    }
 }
