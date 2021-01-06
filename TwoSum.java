29f5e328b3bce6667f0019389339c6068b6043bf
Gabe Corella <69766707+gblcorella@users.noreply.github.com>
2021-01-05 22:39:05 -0500

    two sum adddition

--- TwoSum.java
+++ TwoSum.java
@@ -8,6 +8,46 @@
  *
  * @author gabecorella
  */
+
+import java.util.*;
 public class TwoSum {
     
+    // Two Sum I
+    
+    public int[] twoSum(int[] nums, int target){
+        int[] result = new int[2];
+        HashMap<Integer, Integer> map = new HashMap();
+        for(int i = 0; i < nums.length; i++){
+            int diff = target - nums[i];
+            
+            if(map.containsKey(diff)){
+                result[0] = i;
+                result[1] = map.get(diff);
+            }
+            map.put(nums[i],i);
+        }
+        return result;
+    }
+    
+    
+    // Two Sum II
+    
+    public int[] twoSumII(int[] nums, int target){
+        if(nums.length == 0 || nums == null) return new int[] {-1,-1};
+        
+        int a_pointer = 0; 
+        int b_pointer = nums.length - 1; 
+        
+        while(a_pointer <= b_pointer){
+            int sum = nums[a_pointer]+ nums[b_pointer];
+            if(sum > target){
+                b_pointer--;
+            } else if(sum < target){
+                a_pointer++;
+            } else {
+                return new int[]{a_pointer + 1, b_pointer+1};
+            }
+        }
+        return new int[]{a_pointer+1, b_pointer + 1};
+    }
 }
