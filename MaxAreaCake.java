d53ec47cbf982e2bb29abea83543a85025d600e1
Gabe Corella <69766707+gblcorella@users.noreply.github.com>
2021-01-18 22:34:47 -0500

    daily commit

---
+++ MaxAreaCake.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gabecorella
+ */
+public class MaxAreaCake {
+    public int maxArea(int h, int w, int[] vcuts, int[] hcuts){
+        return (getDist())
+    }
+    
+    private int getDist(int res, int[] cuts){
+        
+    }
+}
--- VowelDynamic.java
+++ VowelDynamic.java
@@ -10,4 +10,24 @@
  */
 public class VowelDynamic {
     
+    
+    /*
+        Given an integer n, return the number of strings of length n that consist only of vowels (a, e, i, o, u) and 
+        are lexicographically sorted.
+
+        A string s is lexicographically sorted if for all valid i,
+        s[i] is the same as or comes before s[i+1] in the alphabet.
+    
+        Input: n = 1
+        Output: 5
+        Explanation: The 5 sorted strings that consist of vowels only are
+        ["a","e","i","o","u"].
+    */
+        public int countVowelStrings(int n) {
+        int[][] dp = new int[n + 1][6];
+        for (int i = 1; i <= n; ++i)
+            for (int k = 1; k <= 5; ++k)
+                dp[i][k] = dp[i][k - 1] + (i > 1 ? dp[i - 1][k] : 1);
+        return dp[n][5];
+      }
 }
