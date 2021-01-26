c84af358c5953fb83e0e0dce8868b8f0d76d8cd7
Gabe Corella <69766707+gblcorella@users.noreply.github.com>
2021-01-25 23:47:38 -0500

    asda

--- InorderSuccessor.java
+++ InorderSuccessor.java
@@ -8,6 +8,47 @@
  *
  * @author gabecorella
  */
+
+import java.util.*;
+
 public class InorderSuccessor {
     
+    
+    // Given a binary search tree and a node in it, find the in-order successor of that node in the BST.
+    
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p){
+        // Error / Type checking
+        
+        if(root == null || p == null){
+            return null;
+        }
+        
+        if(p.right != null){
+            return findMin(p.right);
+        }
+        // Case 2:
+        TreeNode successor = null;
+        TreeNode q = root;
+        
+        while(q != null){
+            if(q.val > p.val){
+                successor = q;
+                q = q.left;
+            } else if (q.val < p.val){
+                q = q.right;
+            } else {
+                break;
+            }
+        }
+        return successor;
+    }
+    
+    
+    public TreeNode findMin(TreeNode root){
+        TreeNode curr = root;
+        while(curr.left != null){
+            curr = curr.left;
+        }
+        return curr;
+    }
 }
