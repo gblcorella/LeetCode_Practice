d0c5436fff62d2062da997398a5a9c75062f4b24
Gabe Corella <69766707+gblcorella@users.noreply.github.com>
2021-01-30 14:33:24 -0500

    Problem of the day, Leetcode

--- WordBreak.java
+++ WordBreak.java
@@ -8,6 +8,28 @@
  *
  * @author gabecorella
  */
+import java.util.*;
 public class WordBreak {
     
+    // Dynamic Programming question 
+    
+     public boolean wordBreak(String s, List<String> wordDict){
+         int n = s.length();
+         boolean[] dp = new boolean[n + 1];
+         
+         dp[0] = true; 
+         int maxLength = 0; 
+         for(String word : wordDict){
+             maxLength = Math.max(maxLength, s.length());
+         }
+         for(int i = 0; i <= n; i++){
+             for(int j = 0; j < i; j++){
+                 if(i - j > maxLength) continue;       
+                 if(dp[j] && wordDict.contains(s.substring(j, i))){
+                     dp[i] = true;
+                 }
+             }
+         }   
+         return dp[n];
+     }
 }
