fd0c9c710115138dd2631243acc57c025e20e390
Gabe Corella <69766707+gblcorella@users.noreply.github.com>
2021-01-31 21:32:43 -0500

    next permutation, leetcode 31

--- MaxCake.java
+++ MaxCake.java
@@ -8,6 +8,23 @@
  *
  * @author gabecorella
  */
+import java.util.*;
 public class MaxCake {
+    
+    public int maxArea(int h, int w, int[] hc, int[] vc) {
+        return (int) ((getMaxDist(h, hc) * getMaxDist(w, vc)) % (Math.pow(10, 9) + 7));
+    }
+    
+    public long getMaxDist(int end, int[] cuts){
+        Arrays.sort(cuts);
+        long from = 0, res = 0;
+        
+        for(int c : cuts){
+            res = Math.max(res, c - from);
+            from = c;
+        }
+        
+        return Math.max(res, end - from);
+    }
     
 }
