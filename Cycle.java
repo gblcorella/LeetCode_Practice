b067d5645d90e4c3e0c302e13eb7b3f358989360
Gabe Corella <69766707+gblcorella@users.noreply.github.com>
2021-02-03 23:12:57 -0500

    daily problem

---
+++ Cycle.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gabecorella
+ */
+public class Cycle {
+    // Use for all cycle problems
+    
+    /*
+    Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+    There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
+    Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+    Return true if there is a cycle in the linked list. Otherwise, return false.
+    */
+    public boolean containsCycle(ListNode head){
+        ListNode slow = head; 
+        ListNode fast = head.next;
+        
+        while(slow != fast){
+            if(fast == null || fast.next == null) return false;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
