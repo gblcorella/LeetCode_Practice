eb78cf25eb89dd25bc8cfe684d27f8bf7e6fac57
Gabe Corella <69766707+gblcorella@users.noreply.github.com>
2021-02-08 13:58:52 -0500

    Practice problems review

--- SimplifyPath.java
+++ SimplifyPath.java
@@ -8,6 +8,24 @@
  *
  * @author gabecorella
  */
+import java.util.*;
+
 public class SimplifyPath {
-    
+
+    public String simplifyPath(String path) {
+        Deque<String> stack = new LinkedList<>();
+        Set<String> skip = new HashSet<>(Arrays.asList("..", ".", ""));
+        for (String dir : path.split("/")) {
+            if (dir.equals("..") && !stack.isEmpty()) {
+                stack.pop();
+            } else if (!skip.contains(dir)) {
+                stack.push(dir);
+            }
+        }
+        String res = "";
+        for (String dir : stack) {
+            res = "/" + dir + res;
+        }
+        return res.isEmpty() ? "/" : res;
+    }
 }
--- TreeRightSide.java
+++ TreeRightSide.java
@@ -8,6 +8,35 @@
  *
  * @author gabecorella
  */
+
+import java.util.*; 
+
 public class TreeRightSide {
+
+        
+    // print tree from the right side 
+    // Level Order traversal with condition regarding size
     
+    public List<Integer> rightView(TreeNode root){
+        List<Integer> result = new ArrayList();
+        Queue<TreeNode> queue = new LinkedList();
+        
+        if(root == null) return result;
+        
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                TreeNode current = queue.remove();
+                if(i == size - 1) result.add(current.val);
+                if(current.left != null){
+                    queue.add(current.left);
+                }
+                if(current.right != null){
+                    queue.add(current.right);
+                }
+            }
+        }
+        return result;
+    }
 }
