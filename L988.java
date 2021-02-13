9efc742c158bde8a6a2b03ad74726aeff132e0e5
Gabe Corella <69766707+gblcorella@users.noreply.github.com>
2021-02-12 20:31:16 -0500

    DFS Backtracking

---
+++ L988.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gabecorella
+ */
+public class L988 {
+    
+    /*
+    Given the root of a binary tree, each node has a value from 0 to 25 representing the letters 'a' to 'z':
+        a value of 0 represents 'a', a value of 1 represents 'b', and so on.
+
+    Find the lexicographically smallest string that starts at a leaf of this tree and ends at the root.
+
+    (As a reminder, any shorter prefix of a string is lexicographically smaller: for example, "ab" is 
+    lexicographically smaller than "aba".  A leaf of a node is a node that has no children.)
+    */
+    
+    public String smallestFromLeaf(TreeNode root){
+        if(root == null) return null;
+        String[] result = new String[]{null};
+        dfsBacktrack(root, new StringBuilder(), result);
+        return result[0]; 
+    }
+    
+    
+    public void dfsBacktrack(TreeNode root, StringBuilder sb, String[] result){
+        if(root == null) return;
+        
+        sb.append((char) ('a' + root.val));
+        
+        if(root.left == null && root.right == null){
+            String coordinate = new StringBuilder(sb).reverse().toString();
+            if(result[0] == null || result[0].compareTo(coordinate) > 0){
+                result[0] = coordinate;
+            }
+        } else{
+            
+        }
+        sb.setLength(sb.length() - 1);
+    }
+}
--- PeekingInteger.java
+++ PeekingInteger.java
@@ -1,3 +1,6 @@
+
+import java.util.Iterator;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +11,45 @@
  *
  * @author gabecorella
  */
-public class PeekingInteger {
+public class PeekingInteger implements Iterator<Integer>{
+    /*
+    Given an Iterator class interface with methods: next() and hasNext(), design and implement a PeekingIterator that support the peek() operation -- 
+    it essentially peek() at the element that will be returned by the next call to next()
+    Example:
+
+    Assume that the iterator is initialized to the beginning of the list: [1,2,3].
+
+    Call next() gets you 1, the first element in the list.
+    Now you call peek() and it returns 2, the next element. Calling next() after that still return 2. 
+    You call next() the final time and it returns 3, the last element. 
+    Calling hasNext() after that should return false.  
+    */
+    
+    private Integer next = null;
+    private Iterator<Integer> iter; 
+    
+    
+    public PeekingInteger(Iterator<Integer> iterator){
+        iter = iterator;
+        if(iter.hasNext()){
+            next = iter.next();
+        }
+        
+    }
+    
+    public Integer peek(){
+        return next;
+    }
+ 
+    @Override
+    public boolean hasNext(){
+        return next != null;
+    }
+    @Override
+    public Integer next(){
+        Integer res = next; 
+        next = iter.hasNext() ? iter.next() : null;
+        return res;
+    }
     
 }
